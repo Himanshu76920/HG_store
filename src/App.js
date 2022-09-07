2511166f1a5b680f10dc4569907b72c840bb6914
randhirsngh057 <mailboxofdesigner@gmail.com>
2022-09-07 20:07:11 +0530

    created dadashboard page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Route from './pages/route'
 import Header from './component/header/Header';
 import './App.css';
+import Styl from './css/Variables.css'
 function App() {
   return (
     <div className="App">
--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import Header from '../../component/header/Header';
 import LeftMenu from '../../component/sidebar/LeftMenu';
 import Style from './Dashboard.scss';
+const Carvalue=[
+{cardname: "Sale", cardprice:12200, cardicon: 'club_night_day'},
+{cardname: "vdvdgdfg", cardprice:12200, cardicon: 'gfdgfdgdf'}
+
+    
+]
 class Dashboard extends Component {
     render() {
         return (
@@ -12,7 +18,16 @@ class Dashboard extends Component {
                 <LeftMenu/>
                 </div>
                 <div className='dadashboard-item'>
-                Dashboard
+                    <div className='small-Card-row'>
+                       {
+                         Carvalue.map((cardinfo)=>
+                         <div className={"small-card " + (cardinfo.cardicon)}>{cardinfo.cardname}</div>
+                        )
+                       }
+                        
+                        {/* <div className='small-card blue-card'>fdgfg</div>
+                        <div className='small-card orange-card'>fdgfg</div> */}
+                    </div>
                 </div>
                 </div>
             </div>
