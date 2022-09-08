2c4e9f57abeb321c6119b195c398fc6fc119c2ba
Arun Kumar <75412113+aruncodemetrics@users.noreply.github.com>
2022-09-08 11:11:14 +0530

    ENV file implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Route from './pages/route'
 import './App.css';
 function App() {
+  console.log(`Api Base RUL: ${process.env.REACT_APP_API_BASE_URL}`)
   return (
     <div className="App">
    <Route />
